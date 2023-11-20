b0cb4236ee23ad7fce380c615e2462be30777954
Hernán Ezequiel Novara <her.novara@hotmail.com>
2023-11-20 18:20:13 -0300

    Otro selector EJ1

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 const title = document.getElementById("mainTitle");
 console.log(title.innerHTML)
+
+// const title2 = document.querySelector("h1"); // Otro selector
+// console.log(title2.innerHTML)
