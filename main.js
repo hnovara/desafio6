5e2fe451b339c94418ef92bdd711d014c50302d0
Victoria Sánchez <vsanchezailen@gmail.com>
2023-11-22 19:21:50 -0300

    Sumé estilos CSS

--- main.js
+++
@@ -1,5 +0,0 @@
-const title = document.getElementById("mainTitle");
-console.log(title.innerHTML)
-
-// const title2 = document.querySelector("h1"); // Otro selector
-// console.log(title2.innerHTML)
