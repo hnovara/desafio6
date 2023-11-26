bb43bd22e2dde90f60c7bffd41b39ad7b1ce7b1a
Hernán Ezequiel Novara <her.novara@hotmail.com>
2023-11-26 20:22:31 -0300

    Ejercicio 3 mayusculas

--- js/main.js
+++ js/main.js
@@ -4,19 +4,38 @@ console.log(title.innerHTML);
 // const title2 = document.querySelector("h1"); // Otro selector
 // console.log(title2.innerHTML)
 
-
+// function createFullName(HTMLList) {
+//     let fullName = "";
+//     for (let index = 0; index < HTMLList.length; index++) {
+//         const element = HTMLList[index];
+//         if (element.tagName === "DD" && element.innerText) {
+//             if (fullName) {
+//                 fullName = fullName.concat(" ")
+//             }
+//             fullName = fullName.concat(`${element.innerText}`);
+//         }
+//     }
+//     return `"${fullName}"`;
+// }
 // Ejercicio 3
 
 function createFullName(HTMLList) {
     let fullName = "";
+    let contadorCampo = 1;
     for (let index = 0; index < HTMLList.length; index++) {
         const element = HTMLList[index];
         if (element.tagName === "DD" && element.innerText) {
             if (fullName) {
                 fullName = fullName.concat(" ")
             }
-            fullName = fullName.concat(`${element.innerText}`);
+            if (contadorCampo<=2) {
+                fullName = fullName.concat(`${element.innerText}`);
+            }
+            else {
+                fullName = fullName.concat(`${element.innerText.toUpperCase()}`);  
+            }   
         }
+        contadorCampo++;
     }
     return `"${fullName}"`;
 }
