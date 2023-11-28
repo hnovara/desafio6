85a16477b55a9d644d2a7fe13335e90c8ee12bc1
Victoria Sánchez <vsanchezailen@gmail.com>
2023-11-28 18:48:16 -0300

    Cambios

--- js/main.js
+++ js/main.js
@@ -49,4 +49,51 @@ function showList(list) {
 
  console.log(`-----\n${showList(divList)}-----`)
 
- 
+
+// Ejercicio 4
+
+// Obtener los elementos de los nombres
+const name1 = document.getElementById("firstName").textContent;
+const name2 = document.getElementById("secondName").textContent;
+const name3 = document.getElementById("firstName2").textContent;
+const name4 = document.getElementById("secondName2").textContent;
+
+// Comparar los nombres ---> Evaluar todas las combinaciones posibles
+
+function compareNames() {
+    if (name1 === name2 || name1 === name3 || name1 === name4 ||
+        name2 === name3 || name2 === name4 ||
+        name3 === name4) {
+        prompt("Hay coincidencia en los nombres.\nIngrese un color para destacar los nombres"); // Almacenar la respuesta en una variable */
+        console.log("Hay coincidencia en los nombres");
+    } else {
+        console.log("No hay coincidencia en los nombres");
+    }
+}
+
+compareNames();
+
+/* --------------------------------- */
+
+/* 
+const setColor = (string, color) => `<span style = 'color: '${color};'>${string}</span>`;
+
+//dd
+element.innerHTML = "Victoria <span style = 'color: red;'>Sánchez</span>";
+
+setRed('Sánchez', 'red');
+ */
+
+/* --------------------------------- */
+
+// Comparar los apellidos
+
+const confirmLastName = confirm('¿Desea comparar los apellidos?');
+
+if (confirmLastName) {
+    // Realizar la misma lógica pero con los apellidos;
+} else {
+
+}
+
+prompt('hola')
