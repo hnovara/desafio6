b12804c1a615c009bcb41cf03ec106830c7ae4d8
Hernán Ezequiel Novara <her.novara@hotmail.com>
2023-11-29 23:39:50 -0300

    Ejercicio 5 con colores

--- js/main.js
+++ js/main.js
@@ -58,47 +58,51 @@ const name2 = document.getElementById("secondName").textContent;
 const name3 = document.getElementById("firstName2").textContent;
 const name4 = document.getElementById("secondName2").textContent;
 
-// Comparar los nombres ---> Evaluar todas las combinaciones posibles
-
-// function compareNames() {
-//     if (name1 === name2 || name1 === name3 || name1 === name4 ||
-//         name2 === name3 || name2 === name4 ||
-//         name3 === name4) {
-//         prompt("Hay coincidencia en los nombres.\nIngrese un color para destacar los nombres"); // Almacenar la respuesta en una variable */
-//         console.log("Hay coincidencia en los nombres");
-//     } else {
-//         console.log("No hay coincidencia en los nombres");
-//     }
-// }
-
-// compareNames();
-
-
 function compareNames() {
     let match1 = "";
     let match2 = "";
+    let match3 = "";
+    let match4 = "";
     let n1 = "";
-    let n2 = "";
     
-    if (name1 === name3 || name1 === name4) {
+    if (name1 === name3) {
         const match1 = name1; 
         console.log(`Hay coincidencia en el nombre ${match1}`);
         n1 = prompt("Hay coincidencia en el nombre\nIngrese un color para destacar el nombre");
+        firstName.style=`color: ${[n1]}`
+        firstName2.style=`color: ${[n1]}`
         // console.log(n1)
-       } else if ((name2 === name3 || name2 === name4) && (name2)) {
-        const match2 = name2; 
+        } else if (name1 === name4) {
+        const match2 = name1; 
         console.log(`Hay coincidencia en el nombre ${match2}`);
-        n2 = prompt("Hay coincidencia en el nombre\nIngrese un color para destacar el nombre");
-        // console.log(n2)
-       } else {
+        n1 = prompt("Hay coincidencia en el nombre\nIngrese un color para destacar el nombre");
+        firstName.style=`color: ${[n1]}`
+        secondName2.style=`color: ${[n1]}`
+        // console.log(n1)
+        } else if ((name2 === name3) && (name2)) {
+        const match3 = name2; 
+        console.log(`Hay coincidencia en el nombre ${match3}`);
+        n1 = prompt("Hay coincidencia en el nombre\nIngrese un color para destacar el nombre");
+        secondName.style=`color: ${[n1]}`
+        firstName2.style=`color: ${[n1]}`
+        // console.log(n1)
+        } else if ((name2 === name4) && (name2)) {
+        const match4 = name2; 
+        console.log(`Hay coincidencia en el nombre ${match4}`);
+        n1 = prompt("Hay coincidencia en el nombre\nIngrese un color para destacar el nombre");
+        secondName.style=`color: ${[n1]}`
+        secondName2.style=`color: ${[n1]}`
+        // console.log(n1)
+        } else {
         console.log("No hay coincidencias en los nombres");
     }
 }
 compareNames();
 
-const confirmLastName = confirm('¿Desea comparar los apellidos?');
 
 // Obtener los elementos de los apellidos
+const confirmLastName = confirm('¿Desea comparar los apellidos?');
+
 const lastName1 = document.getElementById("firstLastName").textContent;
 const lastName2 = document.getElementById("secondLastName").textContent;
 const lastName3 = document.getElementById("firstLastName2").textContent;
@@ -106,42 +110,43 @@ const lastName4 = document.getElementById("secondLastName2").textContent;
 
 if (confirmLastName) {
     function compareLastNames() {
-        let match2 = "";
-        let match3 = "";
-        let n3 = "";
-        let n4 = "";
+        let match5 = "";
+        let match6 = "";
+        let match7 = "";
+        let match8 = "";
+        let n2 = "";
         
-        if (lastName1 === lastName3 || lastName1 === lastName4) {
-            const match2 = lastName1; 
-            console.log(`Hay coincidencia en el apellido ${match2}`);
-            n3 = prompt("Hay coincidencia en el apellido\nIngrese un color para destacar el apellido");
-            // console.log(n1)
-           } else if ((lastName2 === lastName3 || lastName2 === lastName4) && (lastName2)){
-            const match3 = lastName2; 
-            console.log(`Hay coincidencia en el apellido ${match3}`);
-            n4 = prompt("Hay coincidencia en el apellido\nIngrese un color para destacar dl apellido");
+        if (lastName1 === lastName3) {
+            const match5 = lastName1; 
+            console.log(`Hay coincidencia en el apellido ${match5}`);
+            n2 = prompt("Hay coincidencia en el apellido\nIngrese un color para destacar el apellido");
+            firstLastName.style=`color: ${[n2]}`
+            firstLastName2.style=`color: ${[n2]}`
             // console.log(n2)
-           } else {
+            } else if (lastName1 === lastName4) {
+            const match6 = lastName1; 
+            console.log(`Hay coincidencia en el apellido ${match6}`);
+            n2 = prompt("Hay coincidencia en el apellido\nIngrese un color para destacar el apellido");
+            firstLastName.style=`color: ${[n2]}`
+            secondLastName2.style=`color: ${[n2]}`
+            // console.log(n2)
+            } else if ((lastName2 === lastName3) && (lastName2)) {
+            const match7 = lastName2; 
+            console.log(`Hay coincidencia en el apellido ${match7}`);
+            n2 = prompt("Hay coincidencia en el apellido\nIngrese un color para destacar el apellido");
+            secondLastName.style=`color: ${[n2]}`
+            firstLastName2.style=`color: ${[n2]}`
+            // console.log(n2)
+            } else if ((lastName2 === lastName4) && (lastName2)) {
+            const match8 = lastName2; 
+            console.log(`Hay coincidencia en el apellido ${match8}`);
+            n2 = prompt("Hay coincidencia en el apellido\nIngrese un color para destacar el apellido");
+            secondLastName.style=`color: ${[n2]}`
+            secondLastName2.style=`color: ${[n2]}`
+            // console.log(n2)
+            } else {
             console.log("No hay coincidencias en los apellidos");
         }
     }
     compareLastNames();
 }
-
-
-/* --------------------------------- */
-
-/* 
-const setColor = (string, color) => `<span style = 'color: '${color};'>${string}</span>`;
-
-//dd
-element.innerHTML = "Victoria <span style = 'color: red;'>Sánchez</span>";
-
-setRed('Sánchez', 'red');
- */
-
-/* --------------------------------- */
-
-// Comparar los apellidos
-
-
